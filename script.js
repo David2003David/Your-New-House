7795360add25aa544aae80809360b1a2181d448b
David2003David <88198953+David2003David@users.noreply.github.com>
2021-12-10 22:34:41 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrollY > 20){
+//             $('.header').addClass("sticky");
+//         }else{
+//             $('.header').removeClass("sticky");
+//         }
+//     })
+// });
+
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
